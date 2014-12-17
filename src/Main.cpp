c66861ba3190940766e55ee1441726bda8a3987a
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2014-12-16 18:26:00 -0800

    finish command-line arguments

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,12 +3,12 @@
 #include <thread>
 #include <cmath>
 #include <map>
-
 #include <Windows.h>
 #include <assert.h>
 
 #include "FreeTyper.h"
 #include "CharImage.h"
+#include "Options.h"
 
 #include <argtable2.h>
 
@@ -61,18 +61,103 @@ std::pair<std::map<BitmapImage::greyscaleType, char>, int> getCharToGreyscaleMap
 	if(Options::instance().scaleCharValues)
 	{
 		int scalar = 255 / maxGreyscaleValue;
+
+		std::map<BitmapImage::greyscaleType, char> scaledCharacterMap;
+
 		//now that we have our scalar value, scale the other characters
 		for(std::map<BitmapImage::greyscaleType, char>::iterator mapping = greyscaleToCharacterMap.begin(); mapping != greyscaleToCharacterMap.end(); ++mapping)
 		{
-			(*mapping).first *= scalar;
+			scaledCharacterMap[(*mapping).first * scalar] = (*mapping).second ;
 		}
+
+		maxGreyscaleValue *= scalar;
+
+		return std::make_pair(scaledCharacterMap, maxGreyscaleValue);
 	}
+
+	return std::make_pair(greyscaleToCharacterMap, maxGreyscaleValue);
 }
 
 //function which actually does the image conversion
-std::shared_ptr<CharImage> mainProcess()
+std::shared_ptr<CharImage> mainProcess(png::image<png::gray_pixel> & sourceImage)
 {
+	//obtain rasterizer from factory
+	auto typer = FreeTyper::init( Options::instance().blockSize);
 
+	std::cerr << "Building greyscale to character mapping..." << std::endl;
+
+	auto mapPair = getCharToGreyscaleMap(typer);
+
+	std::cerr << "Translating image..." << std::endl;
+
+	//init variable to hold the object
+	std::shared_ptr<CharImage> outputImage = std::make_shared<CharImage>(sourceImage.get_height() /  Options::instance().blockSize, sourceImage.get_width() /  Options::instance().blockSize);
+
+	// for each row in the source image...
+	for(unsigned int blockStartY = 0; blockStartY < sourceImage.get_height(); blockStartY +=  Options::instance().blockSize)
+	{
+		//and each column in each row in the source image...
+		for(unsigned int blockStartX = 0; blockStartX < sourceImage.get_width(); blockStartX +=  Options::instance().blockSize)
+		{
+			if(Options::instance().infoOut)
+			{
+				std::cout << "Processing block (" << std::dec << blockStartX /  Options::instance().blockSize << ", " << blockStartY /  Options::instance().blockSize << ")" << std::endl;
+			}
+
+
+			// read the block's pixels into an object
+			BitmapImage blockPixels(sourceImage, blockStartX, blockStartY,  Options::instance().blockSize,  Options::instance().blockSize);
+
+			BitmapImage::greyscaleType imageGreyscaleValue = blockPixels.greyscaleValue();
+
+			if(Options::instance().infoOut)
+			{
+				std::cout << "block greyscale value:" << std::dec << (int16_t)imageGreyscaleValue << std::endl;
+			}
+
+			//if there is an exact match, our job is easy
+			if(mapPair.first.find(imageGreyscaleValue) != mapPair.first.end())
+			{
+				if(Options::instance().debugOut)
+				{
+					std::cout << "Found an exact match in char " << '\'' << mapPair.first.at(imageGreyscaleValue) << '\'' << std::endl;
+				}
+				//use our best character in the CharImage for this block
+				outputImage->add(blockStartX /  Options::instance().blockSize, blockStartY /  Options::instance().blockSize, mapPair.first.at(imageGreyscaleValue));
+			}
+			//if we are over the max value, we can only go down and don't need to search
+			else if(imageGreyscaleValue > mapPair.second)
+			{
+				outputImage->add(blockStartX /  Options::instance().blockSize, blockStartY /  Options::instance().blockSize, mapPair.first.at(mapPair.second));
+			}
+			else
+			{
+				//try to find a close match by going around the actual value
+				int offsetFromIdeal = 1;
+				while(mapPair.first.find(imageGreyscaleValue + offsetFromIdeal) == mapPair.first.end())
+				{
+					if(Options::instance().debugOut)
+					{
+						std::cout << "Looking for a match offset from the ideal greyscale value by " << offsetFromIdeal << std::endl;
+					}
+					offsetFromIdeal = offsetFromIdeal > 0 ? -offsetFromIdeal : -offsetFromIdeal + 1;
+
+					assert(imageGreyscaleValue + offsetFromIdeal < 255);
+				}
+
+				if(Options::instance().debugOut)
+				{
+					std::cout << "Found a rough match in char " << std::hex << mapPair.first.at(imageGreyscaleValue + offsetFromIdeal) << std::endl;
+				}
+
+				outputImage->add(blockStartX /  Options::instance().blockSize, blockStartY /  Options::instance().blockSize, mapPair.first.at(imageGreyscaleValue + offsetFromIdeal));
+
+			}
+
+		}
+	}
+
+	return outputImage;
 }
 
 //function which parses the command line, makes sure the arguments are valid, and invokes mainProcess().
@@ -84,9 +169,9 @@ int main(int argc, char** argv)
 	struct arg_lit *scaling = arg_lit0("s", "scaling", "enable scaling the darkest character to near-black");
 	struct arg_lit *force = arg_lit0("f", "force", "overwrite output file if it exists");
 	struct arg_lit *help = arg_lit0("h", "help", "list usage options (this)");
-	struct arg_int *blockSize = arg_int0("s","blockSize", "<n>", "set number of pixels (in either axis) of the original image per character");
-	struct arg_file *input = arg_file1(nullptr, nullptr,"<file>", "filename to read png image from");
-	struct arg_file *output = arg_file1(nullptr,nullptr,"<file>", "filename to write ASCII image to");
+	struct arg_int *blockSize = arg_int0("s","blockSize", "<n>", "set number of pixels (in either axis) of the original image per character.  Default 16.");
+	struct arg_file *input = arg_file1(nullptr, nullptr,"<input>", "filename to read png image from");
+	struct arg_file *output = arg_file1(nullptr,nullptr,"<output>", "filename to write ASCII image to");
 	struct arg_end *end = arg_end(20);
 
 	void * argtable[] = {info, debug, scaling, force, help, blockSize, input, output, end};
@@ -169,81 +254,13 @@ int main(int argc, char** argv)
 		}
 	}
 
-
-	//obtain rasterizer from factory
-	auto typer = FreeTyper::init( Options::instance().blockSize);
-
-	std::cerr << "Building greyscale to character mapping..." << std::endl;
-
-	std::cerr << "Translating image..." << std::endl;
-
-	//init variable to hold the object
-	CharImage outputImage(sourceImage.get_height() /  Options::instance().blockSize, sourceImage.get_width() /  Options::instance().blockSize);
-
-	// for each row in the source image...
-	for(unsigned int blockStartY = 0; blockStartY < sourceImage.get_height(); blockStartY +=  Options::instance().blockSize)
-	{
-		//and each column in each row in the source image...
-		for(unsigned int blockStartX = 0; blockStartX < sourceImage.get_width(); blockStartX +=  Options::instance().blockSize)
-		{
-#ifdef ENABLE_INFO_OUT
-			std::cout << "Processing block (" << std::dec << blockStartX /  Options::instance().blockSize << ", " << blockStartY /  Options::instance().blockSize << ")" << std::endl;
-#endif
-
-
-			// read the block's pixels into an object
-			BitmapImage blockPixels(sourceImage, blockStartX, blockStartY,  Options::instance().blockSize,  Options::instance().blockSize);
-
-			BitmapImage::greyscaleType imageGreyscaleValue = blockPixels.greyscaleValue();
-
-#ifdef ENABLE_INFO_OUT
-			std::cout << "block greyscale value:" << std::dec << (int16_t)imageGreyscaleValue << std::endl;
-#endif
-
-			//if there is an exact match, our job is easy
-			if(greyscaleToCharacterMap.find(imageGreyscaleValue) != greyscaleToCharacterMap.end())
-			{
-#ifdef ENABLE_DEBUG_OUT
-				std::cout << "Found an exact match in char " << '\'' << greyscaleToCharacterMap.at(imageGreyscaleValue) << '\'' << std::endl;
-#endif
-				//use our best character in the CharImage for this block
-				outputImage.add(blockStartX /  Options::instance().blockSize, blockStartY /  Options::instance().blockSize, greyscaleToCharacterMap.at(imageGreyscaleValue));
-			}
-			//if we are over the max value, we can only go down and don't need to search
-			else if(imageGreyscaleValue > maxGreyscaleValue)
-			{
-				outputImage.add(blockStartX /  Options::instance().blockSize, blockStartY /  Options::instance().blockSize, greyscaleToCharacterMap.at(maxGreyscaleValue));
-			}
-			else
-			{
-				//try to find a close match by going around the actual value
-				int offsetFromIdeal = 1;
-				while(greyscaleToCharacterMap.find(imageGreyscaleValue + offsetFromIdeal) == greyscaleToCharacterMap.end())
-				{
-#ifdef ENABLE_DEBUG_OUT
-				std::cout << "Looking for a match offset from the ideal greyscale value by " << offsetFromIdeal << std::endl;
-#endif
-					offsetFromIdeal = offsetFromIdeal > 0 ? -offsetFromIdeal : -offsetFromIdeal + 1;
-
-					assert(imageGreyscaleValue + offsetFromIdeal < 255);
-				}
-
-#ifdef ENABLE_DEBUG_OUT
-				std::cout << "Found a rough match in char " << std::hex << greyscaleToCharacterMap.at(imageGreyscaleValue + offsetFromIdeal) << std::endl;
-#endif
-
-				outputImage.add(blockStartX /  Options::instance().blockSize, blockStartY /  Options::instance().blockSize, greyscaleToCharacterMap.at(imageGreyscaleValue + offsetFromIdeal));
-
-			}
-
-		}
-	}
+	std::shared_ptr<CharImage> charImage = mainProcess(sourceImage);
 
 	std::ofstream outputStream;
 
-	outputStream.open("output.txt");
+	outputStream.open(output->filename[0]);
 
-	outputStream << outputImage;
+	outputStream << "foo";
 
 	outputStream.close();
 
