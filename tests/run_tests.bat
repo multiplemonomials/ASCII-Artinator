30ae27db488c718a43d08aebc7f5a712b2ddefb0
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2015-01-07 20:23:03 -0800

    add tests/sample outputs

---
+++ tests/run_tests.bat
@@ -0,0 +1,6 @@
+﻿aart -f -s --blockSize=8 images\CRW_0044.png tree.txt
+aart -f -s --blockSize=16 images\test.png vangogh.txt
+aart -f -s -i --blockSize=8 images\test.png vangogh_inverted.txt
+aart -f -s --blockSize=4 images\test3.png mountains_detail.txt
+aart -f -s -i --blockSize=4 images\US-wp6.png lakewallpaper-inverted.txt
+aart -f -s --blockSize=8 images\US-wp6.png lakewallpaper.txt
